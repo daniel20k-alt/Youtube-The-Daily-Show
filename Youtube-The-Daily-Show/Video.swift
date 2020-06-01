771fa5ff6612ee9cb4aaf892deead67e2bcb8386
Testing_author <testing_beta@mail.com>
2020-06-01 23:26:46 +0300

    Creating the classes that are going to represent each video that would be displayed by the app.

---
+++ Youtube-The-Daily-Show/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  Youtube-The-Daily-Show
+//
+//  Created by DDDD on 01/06/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
