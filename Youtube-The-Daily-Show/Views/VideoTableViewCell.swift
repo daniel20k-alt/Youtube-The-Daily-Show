e8f7b2c4de58d75ca649021bf6b22f4dc4b0f923
Testing_author <testing_beta@mail.com>
2020-06-22 15:37:52 +0300

    Downloading the thumbnail for each video and displaying the date format in order to be easier to read.
    Organising the project's files into folders for easier reference.
    
    Embedding the StackView into the Prototype cell, as to display the YouTube video thumbnail (1280x720) as well as the Date and Title of the video. Connecting the UI elements to the VideoTableViewCell.

---
+++ Youtube-The-Daily-Show/Views/VideoTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  VideoTableViewCell.swift
+//  Youtube-The-Daily-Show
+//
+//  Created by DDDD on 18/06/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var video:Video?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+}
