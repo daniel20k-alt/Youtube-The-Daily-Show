21c727b5dd52e9b5142b4bda18465af9a068086c
Testing_author <testing_beta@mail.com>
2020-06-22 16:23:26 +0300

    Setting the title, thumbnail and the date label when displaying the video, as well as formatting the date.

--- Youtube-The-Daily-Show/Views/VideoTableViewCell.swift
+++ Youtube-The-Daily-Show/Views/VideoTableViewCell.swift
@@ -29,5 +29,54 @@ class VideoTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    
+    func setCell(_ v:Video) {
+        self.video = v
+        
+        //Ensure there is a video, since the video was force unwrapped below
+        guard self.video != nil else {
+            return
+        }
+        
+        //Setting the title and the date label
+        self.titleLabel.text = video?.title
+        
+        //formatting the date
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        self.dateLabel.text = df.string(from: video!.published)
+        
+        //setting the thumbnail - verifying if not nil
+        guard self.video!.thumbnail != nil else {
+            return
+        }
+        
+        //downloading the thumbnail data
+        let url = URL(string: self.video!.thumbnail)
+        
+        // get the shared URL Session object
+        let session = URLSession.shared
+        
+        //create a data task
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+        
+            if error != nil && data != nil {
+                
+                //check if the cells were recycled properly and display the correct image
+                if url!.absoluteString != self.video?.thumbnail {
+                    return //the cell was recycled properly
+                }
+                
+                //creating the image object
+                let image = UIImage(data: data!)
+                
+                //creating the imageView
+                DispatchQueue.main.async {
+                    self.thumbnailImageView.image = image
+                }
+            }
+        }
+        
+        // starting the data task
+        dataTask.resume()
+    }
 }
